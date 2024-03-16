258cc0fedd9444b22edef5700fd104369e3ce883
subhaeng <153849322+subhaeng@users.noreply.github.com>
2024-03-16 21:12:38 +0530

    Add files via upload

---
+++ Hospital.js
@@ -0,0 +1,20 @@
+let slide = document.querySelectorAll(".patientReview");
+let count = 0;
+
+slide.forEach(function (slides, index) {
+    slides.style.left = `${index * 100}%`
+})
+
+function myFun() {
+    slide.forEach(function (curVal) {
+        curVal.style.transform = `translateX(-${count * 100}%)`
+    })
+}
+setInterval(function () {
+    count++;
+    if (count == slide.length) {
+        count = 0;
+    }
+    myFun()
+}, 2000)
+
